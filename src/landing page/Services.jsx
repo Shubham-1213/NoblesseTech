063cde2bb8ded97b3abf692a6803bbc82f175bb4
pnkkumar123 <pankajkanwar420@gmail.com>
2024-05-15 17:03:10 +0530

    added services section with map function

--- src/landing page/Services.jsx
+++ src/landing page/Services.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Services() {
+  const data = [{detail:'MANAGEMENT CONSULTING',url:'../src/assets/services/consultation.png'},
+                {detail:'INVESTMENT ADVISORY',url:'../src/assets/services/auditor.png'},
+                {detail:'PROGRAM MANAGEMENT',url:'../src/assets/services/programmer.png'},
+                {detail:'PROCUREMENT AND SUPPLY CHAIN',url:'../src/assets/services/trolley.png'},
+]
   return (
-    <div>
-        
-    </div>
+    <Wrapper>
+      {
+        data.map((item,index)=>{
+          const {detail,url} = item;
+          return(
+            <div key={index} className='card' >
+               <img src={url} alt={detail} style={{height:'90px',width:'90px'}} />
+                <p style={{color:'green'}}>{detail}</p>
+
+            </div>
+          )
+        })
+      }       
+    </Wrapper>
   )
 }
-
+const Wrapper = styled.div`
+display:flex;
+flex-direction:row;
+justify-items:space-between;
+.card {
+  width:200px;
+  height:400px;
+  justify-items:center;
+  align-items:center;'
+  margin:auto;
+}
+`
 export default Services
